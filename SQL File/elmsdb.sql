5b8d19440faa07871b80ec534e8b646dc033b6cf
opbobgithub <165013858+opbobgithub@users.noreply.github.com>
2024-04-15 16:03:28 +0530

    sql file commit

--- SQL File/elmsdb.sql
+++ SQL File/elmsdb.sql
@@ -1,31 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Sep 01, 2023 at 01:51 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `elmsdb`
---
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `admin`
---
 
 CREATE TABLE `admin` (
   `id` int(11) NOT NULL,
@@ -34,18 +10,8 @@ CREATE TABLE `admin` (
   `updationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
---
--- Dumping data for table `admin`
---
-
 INSERT INTO `admin` (`id`, `UserName`, `Password`, `updationDate`) VALUES
 (1, 'admin', '5c428d8875d2948607f3e3fe134d71b4', '2023-08-31 11:42:58');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbldepartments`
---
 
 CREATE TABLE `tbldepartments` (
   `id` int(11) NOT NULL,
@@ -65,12 +31,6 @@ INSERT INTO `tbldepartments` (`id`, `DepartmentName`, `DepartmentShortName`, `De
 (3, 'Accounts', 'Accounts', 'ACCNT01', '2023-08-31 14:51:26'),
 (4, 'ADMIN', 'Admin', 'ADMN01', '2023-09-01 11:35:50');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `tblemployees`
---
-
 CREATE TABLE `tblemployees` (
   `id` int(11) NOT NULL,
   `EmpId` varchar(100) NOT NULL,
@@ -89,20 +49,11 @@ CREATE TABLE `tblemployees` (
   `RegDate` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
---
--- Dumping data for table `tblemployees`
---
 
 INSERT INTO `tblemployees` (`id`, `EmpId`, `FirstName`, `LastName`, `EmailId`, `Password`, `Gender`, `Dob`, `Department`, `Address`, `City`, `Country`, `Phonenumber`, `Status`, `RegDate`) VALUES
 (1, '10805121', 'Rahul', 'Kumar', 'rk1995@test.com', 'f925916e2754e5e03f75dd58a5733251', 'Male', '3 August, 1995', 'Information Technology', 'A 123 XYZ Apartment ', 'New Delhi', 'India', '12121212', 1, '2023-08-31 14:56:23'),
 (2, '10235612', 'Garima', 'Yadav', 'grama123@gmail.com', 'f925916e2754e5e03f75dd58a5733251', 'Female', '2 January, 1997', 'Accounts', 'Hno 123 ABC Colony', 'New Delhi', 'India', '7485963210', 1, '2023-08-31 15:02:47'),
 (5, '7856214', 'John', 'Doe', 'jhn12@gmail.com', 'f925916e2754e5e03f75dd58a5733251', 'Male', '3 January, 1995', 'Accounts', 'H no 1', 'Ghaziabad ', 'India', '23232323', 1, '2023-09-01 11:38:23');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tblleaves`
---
 
 CREATE TABLE `tblleaves` (
   `id` int(11) NOT NULL,
@@ -118,19 +69,9 @@ CREATE TABLE `tblleaves` (
   `empid` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
---
--- Dumping data for table `tblleaves`
---
-
 INSERT INTO `tblleaves` (`id`, `LeaveType`, `ToDate`, `FromDate`, `Description`, `PostingDate`, `AdminRemark`, `AdminRemarkDate`, `Status`, `IsRead`, `empid`) VALUES
 (11, 'Casual Leaves', '17/09/2023', '10/09/2023', 'I need leave to visit my home town. ', '2023-08-31 15:06:21', 'Approved', '2023-08-31 20:39:40 ', 1, 1, 1),
 (12, 'Casual Leaves', '15/09/2023', '09/09/2023', 'Need casual leaves for some personal work.', '2023-09-01 11:42:40', 'Leave approved', '2023-09-01 17:13:20 ', 1, 1, 5);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tblleavetype`
---
 
 CREATE TABLE `tblleavetype` (
   `id` int(11) NOT NULL,
@@ -139,23 +80,12 @@ CREATE TABLE `tblleavetype` (
   `CreationDate` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
---
--- Dumping data for table `tblleavetype`
---
-
 INSERT INTO `tblleavetype` (`id`, `LeaveType`, `Description`, `CreationDate`) VALUES
 (1, 'Casual Leaves', 'Casual Leaves', '2023-08-31 14:52:22'),
 (2, 'Earned Leaves', 'Earned Leaves', '2023-08-31 14:52:49'),
 (3, 'Sick Leaves', 'Sick Leaves', '2023-08-31 14:53:15'),
 (4, 'RH (Restricted Leaves)', 'Restricted Leaves', '2023-09-01 11:37:06');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admin`
---
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
@@ -165,60 +95,28 @@ ALTER TABLE `admin`
 ALTER TABLE `tbldepartments`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `tblemployees`
---
 ALTER TABLE `tblemployees`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `tblleaves`
---
 ALTER TABLE `tblleaves`
   ADD PRIMARY KEY (`id`),
   ADD KEY `UserEmail` (`empid`);
 
---
--- Indexes for table `tblleavetype`
---
 ALTER TABLE `tblleaveatype`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `admin`
---
 ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- AUTO_INCREMENT for table `tbldepartments`
---
 ALTER TABLE `tbldepartments`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
---
--- AUTO_INCREMENT for table `tblemployees`
---
 ALTER TABLE `tblemployees`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
---
--- AUTO_INCREMENT for table `tblleaves`
---
 ALTER TABLE `tblleaves`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
---
--- AUTO_INCREMENT for table `tblleavetype`
---
 ALTER TABLE `tblleavetype`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
